b95247044a9b526b63a9200d5daf4d154bad296b
developomp <developomp@gmail.com>
2022-12-30 11:20:44 +0900

    add comment with documentation link

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,7 @@ import preprocess from "svelte-preprocess"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// https://github.com/sveltejs/svelte-p	reprocess
 	preprocess: [
 		preprocess({
 			postcss: true,
