c5d6b162be8bfe16129e9cf78834444b8a74d191
developomp <developomp@gmail.com>
2023-01-27 16:18:34 +0900

    chore: archive repo

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# Exyle.io Website
-
-- submit issues [here](https://github.com/exyleio/exyleio/issues)
-
-## [Documentation](https://exyleio-docs.web.app/docs/projects/website/overview)
+Exyle.io has merged with [War Brokers](https://github.com/War-Brokers)
--- src/lib/util/formatDateTime.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Converts UNIX timestamp (milliseconds) to "MM YYYY" format
- */
-export default (unix_ms_timestamp: number): string => {
-	const date = new Date(unix_ms_timestamp)
-	return date.toLocaleDateString("en-US", { month: "long", year: "numeric" })
-}
